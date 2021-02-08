65ce4939015776fa588fec8d294da9866ba08780
bwlynch <benwlynch@gmail.com>
2021-02-07 19:26:16 -0500

    Basic functionality complete, pre-cleanup

--- fetch-data.js
+++ fetch-data.js
@@ -10,7 +10,9 @@ dotenv.config();
 function makeAPICall(url, options, month, year) {
   fetch(url, options)
     .then( res => res.json() )
-    .then(data => fs.writeFileSync('./data/data-' + year + '-' + month + '.json', JSON.stringify(data)));
+    .then(data => {return data;})
+	//.then(data => dataArray[year + month] = data);
+	    //fs.writeFileSync('./data/data-' + year + '-' + month + '.json', JSON.stringify(data)));
 }
 
 //Gets current date and converts it to months by multiplying year by 12
@@ -18,10 +20,11 @@ var date = new Date();
 var currentMonth = date.getMonth();
 var currentYear = date.getFullYear();
 var totalMonths = (currentYear * 12) + currentMonth;
-var startingYear = (process.env.STARTING_YEAR != null || process.env.STARTING_YEAR != '') ? process.env.STARTING_YEAR : 2018;
+var startingYear = (process.env.STARTING_YEAR != null || process.env.STARTING_YEAR != '') ? process.env.STARTING_YEAR : 2010;
 var fixedMonth;
 var apiKey = process.env.API_KEY;
-
+var dataArray = [];
+var dataset = [];
 
 //Gets data for every month from January of startingYear until the current month and writes it to a file
 for (let i = (startingYear * 12); i <= totalMonths; i++) {
@@ -42,5 +45,72 @@ for (let i = (startingYear * 12); i <= totalMonths; i++) {
     fixedMonth = '0' + tempMonth;
   }
 
-  makeAPICall(url, options, fixedMonth, tempYear);
+  dataset.push(fetch(url, options)
+    .then( res => res.json() )
+    .then(data => data))
+          //.then(data => dataArray[year + month] = data);
+              //fs.writeFileSync('./data/data-' + year + '-' + month + '.json', JSON.stringify(data)));
 }
+	
+//  dataArray.push(datatest)
+//}
+
+
+var obj = {
+   table: []
+};
+
+Promise.all(dataset).then(data => {
+  //console.log(data);
+  for (var j in data) {
+  //console.log(data[j].results.votes);
+  for (var i in data[j].results.votes) {
+    demYes = data[j].results.votes[i]['democratic'].yes,
+    demNo = data[j].results.votes[i]['democratic'].no,
+    repYes = data[j].results.votes[i]['republican'].yes,
+    repNo = data[j].results.votes[i]['republican'].no
+    if ((Math.abs(.5 - demYes/(demYes + demNo)) < .25) && (Math.abs(.5 - repYes/(repYes + repNo)) < .25)) {
+      obj.table.push({'title': data[j].results.votes[i].bill.title,
+       'date': data[j].results.votes[i].date,
+       'number': data[j].results.votes[i].bill.number,
+       'chamber': data[j].results.votes[i].chamber,
+       'billDescription': data[j].results.votes[i].description,
+       'url': data[j].results.votes[i].url,
+       'democraticYes': data[j].results.votes[i]['democratic'].yes,
+       'democraticNo': data[j].results.votes[i]['democratic'].no,
+       'republicanYes': data[j].results.votes[i]['republican'].yes,
+       'republicanNo': data[j].results.votes[i]['republican'].no});
+    }
+
+
+
+    /*//console.log(i + ' - Title: ' + data.results.votes[i].bill.title + '\n');
+    let demSplit = checkForSplits(data.results.votes[i], 'democratic');
+    let repSplit = checkForSplits(data.results.votes[i], 'republican');
+    if (demSplit && repSplit) {
+      console.log('Date: ' + data.results.votes[i].date);
+      console.log('Title: ' + data.results.votes[i].bill.title);
+      console.log('Number: ' + data.results.votes[i].bill.number);
+      console.log('Chamber: ' + data.results.votes[i].chamber);
+      console.log('Bill Description: ' + data.results.votes[i].description);
+      console.log('URL: ' + data.results.votes[i].url);
+      console.log('\nDemocratic Yes: ' + data.results.votes[i]['democratic'].yes);
+      console.log('Democratic No: ' + data.results.votes[i]['democratic'].no);
+      console.log('Republican Yes: ' + data.results.votes[i]['republican'].yes);
+      console.log('Republican No: ' + data.results.votes[i]['republican'].no);
+
+      console.log('\n\n\n');
+    }*/
+  }
+  //console.log(obj);
+ }
+ var newJSON = JSON.stringify(obj);
+ console.log(newJSON);
+ fs.writeFile('./src/data.json', newJSON, function(err) {
+    if (err) throw err;
+    console.log('complete');
+    });
+});
+
+//var newJSON = JSON.stringify(obj);
+//console.log(newJSON);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import data from './data.json';
+
+
+/*class Main extends React.Component {
+  render() {
+    return (
+      <h1>Title</h1>
+      <div>abc
+      </div>
+    );
+  }
+}*/
+
+
+
+class Main extends React.Component {
+  /*constructor(props) {
+    super(props);
+    //this.fillIn = this.fillIn.bind(this);
+  }*/
+	
+  render() {
+    /*let obj = {};
+    fs.readFile('./data/data.json', 'utf8', function readFileCallback(err, data){
+    if (err){
+        console.log(err);
+    } else {
+    obj = JSON.parse(data);
+    }
+    })*/
+    //let dataObj = JSON.parse(data);
+    //console.log(dataObj);
+    //console.log(dataObj[1]);
+    //let admins = require('./data.json');
+    //console.log(data[0][0]);
+    let everything = [];
+    for (let abc in data) {
+      console.log('abc: ' + abc);
+      for (let def in data[abc]) {
+	console.log(data[abc][def].title);
+        if (data[abc][def].title != null) {
+	everything.unshift(<p><b>{data[abc][def].chamber} {data[abc][def].number}</b> <a href={data[abc][def].url}>{data[abc][def].title}</a><br/>
+        {data[abc][def].billDescription}<br/>
+        {data[abc][def].date}<br/>
+        Democratic Votes: Yes {data[abc][def].democraticYes},
+        No {data[abc][def].democraticNo}<br/>
+        Republican Votes: Yes {data[abc][def].republicanYes},
+        No {data[abc][def].republicanNo}
+		<br/><br/><br/></p>);
+	}
+      }
+    }
+    let abcd = 'abc'//dataObj[1];
+    return (
+      <div><h1>Votes that Split the Parties</h1>
+      <p>The increased partisanship in U.S. politics has led to an increase in party line votes where each party votes more or less as a bloc. However there are still some issues that divide the parties. Below is a list of every congressional vote since 2010 where both parties were split, defined as instances where at least a quarter of each party voted differently from the majority of the party.</p><br/>
+      <div>{everything}</div></div>
+    );
+  }
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Main />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
